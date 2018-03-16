82338c3898f8cb648951692f49a3da34ad50dc6d
Shrey Upadhyay <shrey.yay@gmail.com>
2018-03-17 00:37:03 +0530

    Add files via upload

---
+++ wogma/pipelines.py
@@ -0,0 +1,41 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   # -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import scrapy
+import hashlib
+from urllib.parse import quote
+
+
+class ScreenshotPipeline(object):
+    """Pipeline that uses Splash to render screenshot of
+    every Scrapy item."""
+
+    SPLASH_URL = "http://localhost:8050/render.png?url={}"
+
+    def process_item(self, item, spider):
+        encoded_item_url = quote(item["url"])
+        screenshot_url = self.SPLASH_URL.format(encoded_item_url)
+        request = scrapy.Request(screenshot_url)
+        dfd = spider.crawler.engine.download(request, spider)
+        dfd.addBoth(self.return_item, item)
+        return dfd
+
+    def return_item(self, response, item):
+        if response.status != 200:
+            # Error happened, return item.
+            return item
+
+        # Save screenshot to file, filename will be hash of url.
+        url = item["url"]
+        url_hash = hashlib.md5(url.encode("utf8")).hexdigest()
+        filename = "{}.png".format(url_hash)
+        with open(filename, "wb") as f:
+            f.write(response.body)
+
+        # Store filename in item.
+        item["screenshot_filename"] = filename
+        return item
